db4a2a7fcafb61e5d00895cf277e66aae4785bd0
Peter Elliott <p.elliott332@gmail.com>
2026-06-26 12:20:30 -0600

    Dev Server: rebuild on start

--- dev-server.sh
+++ dev-server.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
-set -e
+
+./framework build site/ build/
 
 python3 -m http.server 8000 -d build &
 serverpid=$!
 trap "kill $serverpid" EXIT
 
-while inotifywait -r -e modify,create,delete,move site
+while inotifywait -r -e modify,create,delete,move site shared framework-src/ framework
 do
     ./framework build site/ build/
 done
