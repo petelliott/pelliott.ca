42a820aa1f959fbea9d7487c936af9c8b7c0c870
Peter Elliott <p.elliott332@gmail.com>
2026-07-05 21:41:39 -0600

    support rendering markdown with pandoc

--- site/site.scm
+++ site/site.scm
@@ -1,4 +1,17 @@
 (use-modules (schingle template mustache)
+             (ice-9 popen)
+             (ice-9 textual-ports)
              (parameters))
 
+(define (markdown->html content)
+  (define-values (in out pids) (pipeline '(("pandoc" "-f" "markdown" "-t" "html"))))
+  (display content out)
+  (close-port out)
+  (define html (get-string-all in))
+  (close-port in)
+  (waitpid (car pids))
+  html)
+
+
+(add-renderer ".md" markdown->html)
 (add-renderer ".mustache" (mustache-renderer *global-template-params*))
