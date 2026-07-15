b20265dc1c58189cb2b87085c4d88d61ef0d6f80
Peter Elliott <p.elliott332@gmail.com>
2026-07-15 16:04:53 -0600

    no clue how these got like this

--- site/status.scm
+++
@@ -1,9 +0,0 @@
-(use-modules (parameters)
-             (feedfile))
-
-(define statuses (load-feedfile "STATUS"))
-
-;; END STATUSES
-
-(add-global-template-parameter 'status `((current . ,(car (*statuses*)))
-                                         (old . ,(list->vector (cdr (*statuses*))))))
--- site/status/status.scm
+++ site/status/status.scm
@@ -1,18 +1,9 @@
-(use-modules (parameters))
+(use-modules (parameters)
+             (feedfile))
 
-(define *statuses* (make-parameter '()))
-
-(define (status date text)
-  (*statuses* (cons `((date . ,date)
-                      (text . ,text))
-                    (*statuses*))))
-
-
-;; BEGIN STATUSES
-
-(status "2026-06-12" "working on my website")
+(define statuses (load-feedfile "STATUS"))
 
 ;; END STATUSES
 
-(add-global-template-parameter 'status `((current . ,(car (*statuses*)))
-                                         (old . ,(list->vector (cdr (*statuses*))))))
+(add-global-template-parameter 'status `((current . ,(car statuses))
+                                         (old . ,(list->vector (cdr statuses)))))
